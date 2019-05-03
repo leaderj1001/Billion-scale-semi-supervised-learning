ab8312ad4fd7e4cf4d32c8b7ab3a4fdd0b3b56bf
myeongjun <leaderj1001@gmail.com>
2019-05-03 20:02:01 +0900

    [Create] resnet50, checkpoint weights(75.75%)

---
+++ main.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import _LRScheduler
+
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR
+
+import argparse
+import os
+
+import time
+from tqdm import tqdm
+
+from model import resnet50
+from preprocess import load_data
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+# reference,
+# https://github.com/weiaicunzai/pytorch-cifar100/blob/master/utils.py Thank you !!
+class WarmUpLR(_LRScheduler):
+    """warmup_training learning rate scheduler
+    Args:
+        optimizer: optimzier(e.g. SGD)
+        total_iters: totoal_iters of warmup phase
+    """
+
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """we will use the first m batches, and set the learning
+        rate to base_lr * m / total_iters
+        """
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def train(model, train_loader, optimizer, criterion, epoch, args):
+    model.train()
+    step = 0
+    train_loss = 0
+    train_acc = 0
+    for data, target in tqdm(train_loader, desc="epoch " + str(epoch), mininterval=1):
+        adjust_learning_rate(optimizer, epoch, args)
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.data
+        y_pred = output.data.max(1)[1]
+
+        acc = float(y_pred.eq(target.data).sum()) / len(data) * 100.
+        train_acc += acc
+        step += 1
+        if step % 100 == 0:
+            print("[Epoch {0:4d}] Loss: {1:2.3f} Acc: {2:.3f}%".format(epoch, loss.data, acc), end='')
+            for param_group in optimizer.param_groups:
+                print(",  Current learning rate is: {}".format(param_group['lr']))
+
+    # layer_visualize(data[0], c2[0], c3[0], c4[0], epoch)
+    length = len(train_loader.dataset) // args.batch_size
+    return train_loss / length, train_acc / length
+
+
+def get_test(model, test_loader):
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for data, target in tqdm(test_loader, desc="evaluation", mininterval=1):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            prediction = output.data.max(1)[1]
+            correct += prediction.eq(target.data).sum()
+
+    acc = 100. * float(correct) / len(test_loader.dataset)
+    return acc
+
+
+def main():
+    parser = argparse.ArgumentParser('parameters')
+
+    parser.add_argument('--epochs', type=int, default=100, help='number of epochs, (default: 100)')
+    parser.add_argument('--learning-rate', type=float, default=1e-1, help='learning rate, (default: 1e-1)')
+    parser.add_argument('--batch-size', type=int, default=100, help='batch size, (default: 100)')
+    parser.add_argument('--model-mode', type=str, default="CIFAR100", help='CIFAR10, CIFAR100, SMALL_REGIME, REGULAR_REGIME, (default: CIFAR10)')
+    parser.add_argument('--dataset-mode', type=str, default="CIFAR100", help='Which dataset to use? (Example, CIFAR10, CIFAR100, MNIST), (default: CIFAR10)')
+    parser.add_argument('--is-train', type=bool, default=True, help="True if training, False if test. (default: True)")
+    parser.add_argument('--drop-path', type=float, default=0.1, help="regularization by disconnecting between random graphs,")
+    parser.add_argument('--load-model', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    train_loader, test_loader = load_data(args)
+
+    if args.load_model:
+        model = resnet50().to(device)
+        filename = "Best_model_"
+        checkpoint = torch.load('./checkpoint/' + filename + 'ckpt.t7')
+        model.load_state_dict(checkpoint['model'])
+        epoch = checkpoint['epoch']
+        acc = checkpoint['acc']
+        print("Load Model Accuracy: ", acc, "Load Model end epoch: ", epoch)
+    else:
+        model = resnet50().to(device)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=5e-4, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    epoch_list = []
+    test_acc_list = []
+    train_acc_list = []
+    train_loss_list = []
+    max_test_acc = 0
+    if not os.path.isdir("reporting"):
+        os.mkdir("reporting")
+
+    start_time = time.time()
+    with open("./reporting/best_model.txt", "w") as f:
+        for epoch in range(1, args.epochs + 1):
+            # scheduler = CosineAnnealingLR(optimizer, epoch)
+            epoch_list.append(epoch)
+            train_loss, train_acc = train(model, train_loader, optimizer, criterion, epoch, args)
+            test_acc = get_test(model, test_loader)
+            test_acc_list.append(test_acc)
+            train_loss_list.append(train_loss)
+            train_acc_list.append(train_acc)
+            print('Test set accuracy: {0:.2f}%, Best accuracy: {1:.2f}%'.format(test_acc, max_test_acc))
+            f.write("[Epoch {0:3d}] Test set accuracy: {1:.3f}%, , Best accuracy: {2:.2f}%".format(epoch, test_acc, max_test_acc))
+            f.write("\n ")
+
+            if max_test_acc < test_acc:
+                print('Saving..')
+                state = {
+                    'model': model.state_dict(),
+                    'acc': test_acc,
+                    'epoch': epoch,
+                }
+                if not os.path.isdir('checkpoint'):
+                    os.mkdir('checkpoint')
+                filename = "Best_model_"
+                torch.save(state, './checkpoint/' + filename + 'ckpt.t7')
+                max_test_acc = test_acc
+                # draw_plot(epoch_list, train_loss_list, train_acc_list, test_acc_list)
+            time_interval = time.time() - start_time
+            time_split = time.gmtime(time_interval)
+            print("Training time: ", time_interval, "Hour: ", time_split.tm_hour, "Minute: ", time_split.tm_min, "Second: ", time_split.tm_sec)
+            f.write("Training time: " + str(time_interval) + "Hour: " + str(time_split.tm_hour) + "Minute: " + str(time_split.tm_min) + "Second: " + str(time_split.tm_sec))
+            f.write("\n")
+
+
+if __name__ == '__main__':
+    main()
